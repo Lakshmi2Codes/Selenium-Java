0bf23665cf9e9132a42351b1356901699478cd08
Lakshmi-Mohan <lpkay2020@gmail.com>
2026-01-04 21:48:08 -0800

    Add files via upload

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean beta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println(
+				"Browser Name: Chrome" + "\n"+
+			    "Version: " + version + "\n" +
+			    "Developer: " + developer + "\n" +
+			    "Beta: " + beta + "\n" +
+			    "Release Year: " + releaseYear + "\n" +
+			    "Shortcut Key: " + shortcutKey
+			);
+	}
+
+}
